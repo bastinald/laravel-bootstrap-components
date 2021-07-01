dbf14d7fd5e711a5c5578f4de24486195780e742
bastinald <bastinald@icloud.com>
2021-07-01 05:12:18 -0400

    pagi if has

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -12,22 +12,24 @@
     ]);
 @endphp
 
-<div {{ $attributes }}>
-    @if(isset($this) && method_exists($this, 'initializeWithPagination'))
-        <div class="d-block d-lg-none">
-            {{ $links->links('livewire::simple-bootstrap') }}
-        </div>
+@if($links->hasPages())
+    <div {{ $attributes }}>
+        @if(isset($this) && method_exists($this, 'initializeWithPagination'))
+            <div class="d-block d-lg-none">
+                {{ $links->links('livewire::simple-bootstrap') }}
+            </div>
 
-        <div class="d-none d-lg-block">
-            {{ $links->links('livewire::bootstrap') }}
-        </div>
-    @else
-        <div class="d-block d-lg-none">
-            {{ $links->links('pagination::simple-bootstrap-4') }}
-        </div>
+            <div class="d-none d-lg-block">
+                {{ $links->links('livewire::bootstrap') }}
+            </div>
+        @else
+            <div class="d-block d-lg-none">
+                {{ $links->links('pagination::simple-bootstrap-4') }}
+            </div>
 
-        <div class="d-none d-lg-block">
-            {{ $links->links('pagination::bootstrap-4') }}
-        </div>
-    @endif
-</div>
+            <div class="d-none d-lg-block">
+                {{ $links->links('pagination::bootstrap-4') }}
+            </div>
+        @endif
+    </div>
+@endif
