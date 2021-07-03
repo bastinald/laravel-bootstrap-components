5af55581c399cba16f40f20730238115f51a213c
bastinald <bastinald@icloud.com>
2021-07-03 11:04:18 -0400

    icon color

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -1,10 +1,12 @@
 @props([
     'name' => null,
+    'color' => null,
 ])
 
 @php
     $attributes = $attributes->class([
         config('laravel-bootstrap-components.icon_class_prefix') . $name,
+        'text-' . $color => $color,
     ])->merge([
         //
     ]);
